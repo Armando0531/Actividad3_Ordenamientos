dba4245c2e9f9f134788838cad54800fee284367
Armando Enriquez <armandoenriquez0531@gmail.com>
2021-12-07 18:22:36 -0600

    Se creo una clase PruebaEstres con métodos para cada método de ordenamiento

--- src/PruebaOrdenamiento.java
+++ src/PruebaOrdenamiento.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 class MetodosOrdenamiento{
 	
 	int [] contador=new int[3];
@@ -196,6 +199,99 @@ class MetodosOrdenamiento{
 		
 	
 	}
+}
+
+class pruebasEstres{
+	
+	public void pruebaEstres(String op) {
+		int vector1000elementos[]=new int[1000];
+		int vector10000elementos[]=new int[10000];
+		int vector100000elementos[]=new int[100000];
+		int vector1000000elementos[]=new int[1000000];
+		//System.out.println("Acabo el 1");
+		for(int i=0;i<vector1000elementos.length;i++) {
+			vector1000elementos[i]=(int) (Math.random() * 1000 + 1);
+		}
+		//System.out.println("Acabo el 2");
+		for(int i=0;i<vector10000elementos.length;i++) {
+			vector10000elementos[i]=(int) (Math.random() * 10000 + 1);
+		}
+		//System.out.println("Acabo el 3");
+		for(int i=0;i<vector100000elementos.length;i++) {
+			vector100000elementos[i]=(int) (Math.random() * 10000 + 1);
+		}
+		//System.out.println("Acabo el 4");
+		for(int i=0;i<vector1000000elementos.length;i++) {
+			vector1000000elementos[i]=(int) (Math.random() * 100000 + 1);
+		}
+		int numerosDesordenados[]= {55,15,78,99,16,1,112,23,1789};
+		int copi[];
+		if(op.equalsIgnoreCase("1")) {
+			pruebaEstres(numerosDesordenados);
+		}else if (op.equalsIgnoreCase("2")) {
+			pruebaEstres(vector1000elementos);
+		}
+		else if (op.equalsIgnoreCase("3")) {
+			pruebaEstres(vector10000elementos);
+		}
+		else if (op.equalsIgnoreCase("4")) {
+			pruebaEstres(vector100000elementos);
+		}
+		else if (op.equalsIgnoreCase("5")) {
+			pruebaEstres(vector1000000elementos);
+		}
+	}
+	public void pruebaEstres(int vector[]) {
+		MetodosOrdenamiento orden=new MetodosOrdenamiento();
+		long tTnicio,tFin;
+		int copi[];
+		System.out.println("Pruebas estres intercalacion");
+		int cantidad1=0;
+		int cantidad2=0;
+		int c=0;
+		cantidad1=(int) (Math.random() * vector.length);
+		cantidad2=vector.length-cantidad1;
+		int v1[]=new int[cantidad1];
+		int v2[]=new int[cantidad2];
+		for(int i=0;i<cantidad1;i++) {
+			v1[i]=vector[c];
+			c++;
+		}
+		for(int i=0;i<cantidad2;i++) {
+			v2[i]=vector[c];
+			c++;
+		}
+		//System.out.println(Arrays.toString(v1));
+		//System.out.println(Arrays.toString(v2));
+		copi=null;
+		tFin=tTnicio=0;
+		copi=Arrays.copyOf(vector,vector.length-1);
+		v1=orden.ordenamientoMezclaDirecto(v1);
+		v1=orden.ordenamientoMezclaDirecto(v2);
+		tTnicio=System.nanoTime();
+		orden.ordenarIntercalacion(v1, v2);
+		tFin=System.nanoTime();
+		System.out.println("Tardo: "+(tFin-tTnicio));
+		orden.mostrarContador();
+		System.out.println("-------------------- Mezcla directa ---------------------------");
+		copi=null;
+		tFin=tTnicio=0;
+		copi=Arrays.copyOf(vector,vector.length-1);
+		tTnicio=System.nanoTime();
+		orden.ordenamientoMezclaDirecto(copi);
+		tFin=System.nanoTime();
+		System.out.println("Tardo: "+(tFin-tTnicio));
+		orden.mostrarContador();
+		System.out.println("--------------------Mezcla Natural---------------------------");
+		copi=null;
+		tFin=tTnicio=0;
+		copi=Arrays.copyOf(vector,vector.length-1);
+		tTnicio=System.nanoTime();
+		orden.ordenamientoMezclaNatural(copi);
+		tFin=System.nanoTime();
+		System.out.println("Tardo: "+(tFin-tTnicio));
+		orden.mostrarContador();
+		}
 }
 
 public class PruebaOrdenamiento {
